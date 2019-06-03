b52d82ad6ab983a778e162d71c975ea93e124b5c
Sigvart Hovland <sigvart.m@gmail.com>
2019-06-03 03:43:44 +0200

    subsys: net: aws_jobs A minimal AWS Jobs implementation
    This is a minimal AWS Jobs implementation to support FOTA through the
    use of AWS Jobs.
    
    Signed-off-by: Sigvart Hovland <sigvart.m@gmail.com>

---
+++ include/aws_jobs.h
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file aws_jobs.h
+ *  The AWS jobs library provides APIs to:
+ *  - Report status
+ *  - Subscribe to job topics
+ *  - String templates for topics
+ *  - Defines for lengths of topics, status and job IDs
+ *  - Defines for Subscribe message IDs
+ *
+ */
+
+#ifndef AWS_JOBS_H__
+#define AWS_JOBS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <zephyr.h>
+#include <zephyr/types.h>
+#include <net/mqtt.h>
+
+/* @brief The max JOB_ID_LEN according to AWS docs
+ * https://docs.aws.amazon.com/general/latest/gr/aws_service_limits.html
+ */
+#define JOB_ID_MAX_LEN (64)
+#define STATUS_MAX_LEN (12)
+
+/* @brief MQTT message IDs used for identifying subscribe message ACKs */
+#define SUBSCRIBE_ID_BASE (2110)
+#define SUBSCRIBE_NOTIFY (SUBSCRIBE_ID_BASE + 1)
+#define SUBSCRIBE_NOTIFY_NEXT (SUBSCRIBE_ID_BASE + 2)
+#define SUBSCRIBE_GET (SUBSCRIBE_ID_BASE + 3)
+#define SUBSCRIBE_GET_JOB_ID (SUBSCRIBE_ID_BASE + 4)
+#define SUBSCRIBE_JOB_ID_UPDATE (SUBSCRIBE_ID_BASE + 5)
+#define SUBSCRIBE_EXPECTED (SUBSCRIBE_ID_BASE + 10)
+
+/**
+ * @brief String templates for constructing AWS Jobs topics and their max
+ * length
+ */
+#define AWS "$aws/things/"
+#define AWS_LEN (sizeof(AWS) - 1)
+
+#define NOTIFY_TOPIC_TEMPLATE AWS "%s/jobs/notify"
+#define NOTIFY_TOPIC_MAX_LEN (AWS_LEN + CONFIG_CLIENT_ID_LEN + 12)
+
+#define NOTIFY_NEXT_TOPIC_TEMPLATE AWS "%s/jobs/notify-next"
+#define NOTIFY_NEXT_TOPIC_MAX_LEN (AWS_LEN + CONFIG_CLIENT_ID_LEN + 18)
+
+#define GET_TOPIC_TEMPLATE AWS "%s/jobs/get/%s"
+#define GET_TOPIC_LEN (AWS_LEN + CONFIG_CLIENT_ID_LEN + 11)
+
+#define JOB_ID_GET_TOPIC_TEMPLATE AWS "%s/jobs/%s/get/%s"
+#define JOB_ID_GET_TOPIC_MAX_LEN (AWS_LEN +\
+				 CONFIG_CLIENT_ID_LEN +\
+				 JOB_ID_MAX_LEN +\
+				 12)
+
+#define JOB_ID_UPDATE_TOPIC_TEMPLATE AWS "%s/jobs/%s/update"
+#define JOB_ID_UPDATE_TOPIC_MAX_LEN (AWS_LEN +\
+				     CONFIG_CLIENT_ID_LEN +\
+				     JOB_ID_MAX_LEN +\
+				     14)
+
+
+/** @brief Job Execution Status. */
+enum execution_status {
+	QUEUED = 0,
+	IN_PROGRESS,
+	SUCCEEDED,
+	FAILED,
+	TIMED_OUT,
+	REJECTED,
+	REMOVED,
+	CANCELED
+};
+
+/** @brief Mapping of enum to strings for Job Execution Status. */
+const char *execution_status_map[] = {
+	"QUEUED",
+	"IN_PROGRESS",
+	"SUCCEEDED",
+	"FAILED",
+	"TIMED_OUT",
+	"REJECTED",
+	"REMOVED",
+	"CANCELED"
+};
+
+/**
+ * @brief Constructs then subscribes to get topic for AWS IoT jobs for accepted
+ *	  and rejected get topic.
+ *
+ * @param[in] client Connected MQTT client instance.
+ *
+ * @retval 0 If successful otherwise the return code of mqtt_subscribe.
+ *
+ */
+int aws_jobs_subscribe_get(struct mqtt_client *client);
+
+
+/**
+ * @brief Constructs then subscribes to notify-next topic for reciving
+ * AWS IoT jobs.
+ *
+ * @param[in] client Connected MQTT client instance.
+ *
+ * @retval 0 If sucessful otherwise the return code of mqtt_subscribe.
+ *
+ */
+int aws_jobs_subscribe_notify_next(struct mqtt_client *client);
+
+/**
+ * @brief Constructs then subscribes to notify topic for reciving  AWS IoT jobs.
+ *
+ * @param[in] client Connected MQTT client instance.
+ *
+ * @retval 0 If sucessful otherwise the return code of mqtt_subscribe.
+ *
+ */
+int aws_jobs_subscribe_notify(struct mqtt_client *client);
+
+
+/**@brief Subscribe to the expected topics of the AWS documentation
+ * https://docs.aws.amazon.com/iot/latest/developerguide/jobs-devices.html.
+ *
+ * @param[in] client Connected MQTT client instance.
+ * @param[in] notify_next Subscribes to notify next if true and notify if false.
+ */
+int aws_jobs_subscribe_expected_topics(struct mqtt_client *client,
+				       bool notify_next);
+
+
+/**
+ * @brief AWS jobs update job execution status function.
+ *
+ * This implements the minimal requirements for doing an AWS job only
+ * updating status and status details update are supported.
+ *
+ * @param[in] client Initialized and connected MQTT Client instance.
+ * @param[in] job_id The ID of the job which you are updating this will be a
+ *		part of the MQTT topic used to update the job.
+ * @param[in] status The Job execution status.
+ * @param[in] status_details JSON object in stringformat containing additional
+		information. This object can contain up to 10 fields according
+		to the AWS IoT jobs documentation.
+ * @param[in] expected_version The expected job document version this needs to
+ *		be incremented by 1 for every update.
+ * @param[in] client_token This can be an arbitrary value and will be reflected
+ *		in the response to the update.
+ *
+ * @retval 0 If update is published successful otherwise return mqtt_publish
+ *	     error code.
+ *
+ */
+int aws_update_job_execution(struct mqtt_client *client,
+			     u8_t *job_id,
+			     enum execution_status status,
+			     u8_t *status_details,
+			     int expected_version,
+			     u8_t *client_token);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AWS_JOBS_H__ */
+
