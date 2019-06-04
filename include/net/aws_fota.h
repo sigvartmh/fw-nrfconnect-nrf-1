49f62e5d7279d1a6aa25f663aa76ad501ff28580
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-06-04 06:33:38 +0200

    subsys: net: lib: aws_fota: Implementation of FOTA through AWS IoT jobs
    Implements FOTA through the use of the AWS Jobs API for AWS IoT.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>

---
+++ include/net/aws_fota.h
@@ -0,0 +1,52 @@
+/*
+ *Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ *SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ *@brief AWS FOTA library header.
+ */
+
+#ifndef AWS_FOTA_H__
+#define AWS_FOTA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum aws_fota_evt_id {
+	/** AWS FOTA complete and status reported to job document */
+	AWS_FOTA_EVT_DONE,
+	/** AWS FOTA error */
+	AWS_FOTA_EVT_ERROR
+};
+
+typedef void (*aws_fota_callback_t)(enum aws_fota_evt_id evt_id);
+
+/**@brief Initialize the AWS Firmware Over the Air library.
+ *
+ * @param app_version Current version number of the application as a \0
+ *	terminated ASCII string.
+ * @param callback Callback for events generated.
+ *
+ * @retval 0 If successfully initialized.
+ * @retval -EINVAL If any of the input values are invalid.
+ *           Otherwise, a negative value is returned.
+ */
+int aws_fota_init(struct mqtt_client *const client,
+		  const char *app_version,
+		  aws_fota_callback_t cb);
+
+int aws_fota_mqtt_evt_handler(struct mqtt_client *const client,
+			      const struct mqtt_evt *evt);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ *@}
+ */
+
+#endif /*AWS_FOTA_H__ */
---
+++ subsys/net/lib/aws_fota/include/aws_fota_internal.h
@@ -0,0 +1,101 @@
+/*
+ *Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ *SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ */
+
+/**@file
+ *@brief AWS FOTA library header.
+ */
+
+#ifndef AWS_FOTA_INTERNAL_H__
+#define AWS_FOTA_INTERNAL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <zephyr.h>
+#include <zephyr/types.h>
+#include <net/mqtt.h>
+
+/**
+ *Example payload
+{
+  "timestamp": 1559626627,
+  "execution": {
+    "jobId": "8467e3b5-c8b3-4145-8124-eb4c95b21538",
+    "status": "QUEUED",
+    "queuedAt": 1559626593,
+    "lastUpdatedAt": 1559626593,
+    "versionNumber": 1,
+    "executionNumber": 1,
+    "jobDocument": {
+      "operation": "app_fw_update",
+      "fwversion": 293692,
+      "size": 181124,
+      "location": {
+        "protocol": "https:",
+        "host": "fota-update-bucket.s3.eu-central-1.amazonaws.com",
+        "path": "/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190604%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190604T053631Z&X-Amz-Expires=604800&X-Amz-Signature=cd2fb78a66645abce44d851041000f2ece49b01c3298e09d450e09c707726804&X-Amz-SignedHeaders=host"
+      }
+    }
+  }
+}
+*/
+
+struct location_obj {
+	const char *protocol;
+	const char *host;
+	const char *path;
+};
+
+struct job_document_obj {
+	const char *operation;
+	const char *fw_version;
+	int size;
+	struct location_obj location;
+};
+
+struct execution_obj {
+	const char *job_id;
+	const char *status;
+	int queued_at;
+	int last_update_at;
+	int version_number;
+	int execution_number;
+	struct job_document_obj job_document;
+};
+
+struct notify_next_obj {
+	int timestamp;
+	struct execution_obj execution;
+};
+
+struct status_details_obj {
+	const char *next_state;
+};
+
+struct execution_state_obj {
+	const char *status;
+	struct status_details_obj status_details;
+	int version_number;
+};
+
+struct update_response_obj {
+	struct execution_state_obj execution_state;
+	const char* job_document;
+	int timestamp;
+	const char* client_token;
+};
+
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ *@}
+ */
+
+#endif /*AWS_FOTA_INTERNAL_H__ */
