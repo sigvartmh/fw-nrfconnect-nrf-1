83105b97e010c4901384b5403d1220f96bf56fc4
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-10-02 13:38:13 +0200

    subsys: net: lib: fota_download: Change to use dfu context handler
    Change fota_download to use the generic dfu context handler
    implementation for handling the incoming file.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>

--- subsys/net/lib/fota_download/src/fota_download.c
+++ subsys/net/lib/fota_download/src/fota_download.c
@@ -4,24 +4,21 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
-#include <stdint.h>
 #include <zephyr.h>
-#include <flash.h>
-#include <net/download_client.h>
-#include <dfu/mcuboot.h>
-#include <dfu/flash_img.h>
 #include <pm_config.h>
 #include <logging/log.h>
 #include <net/fota_download.h>
+#include <net/download_client.h>
+#include <dfu/dfu_context_handler.h>
 
 LOG_MODULE_REGISTER(fota_download, CONFIG_FOTA_DOWNLOAD_LOG_LEVEL);
 
-static		fota_download_callback_t callback;
-static struct	flash_img_context flash_img;
-static struct	download_client dfu;
+static fota_download_callback_t callback;
+static struct download_client   dlc;
 
 static int download_client_callback(const struct download_client_evt *event)
 {
+	static bool first_fragment = true;
 	int err;
 
 	if (event == NULL) {
@@ -30,59 +27,64 @@ static int download_client_callback(const struct download_client_evt *event)
 
 	switch (event->id) {
 	case DOWNLOAD_CLIENT_EVT_FRAGMENT: {
-		size_t size;
+		size_t file_size;
 
-		err = download_client_file_size_get(&dfu, &size);
+		err = download_client_file_size_get(&dlc, &file_size);
 		if (err != 0) {
 			LOG_ERR("download_client_file_size_get error %d", err);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
-		if (size > PM_MCUBOOT_SECONDARY_SIZE) {
+
+		if (file_size > PM_MCUBOOT_SECONDARY_SIZE) {
 			LOG_ERR("Requested file too big to fit in flash\n");
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return -EFBIG;
 		}
 
-		err = flash_img_buffered_write(&flash_img,
-				(u8_t *)event->fragment.buf,
-				event->fragment.len, false);
+		if (first_fragment) {
+			int img_type = dfu_ctx_img_type(event->fragment.buf,
+						        event->fragment.len);
+
+			err = dfu_ctx_init(img_type);
+			if (err != 0) {
+				LOG_ERR("dfu_ctx_init error %d", err);
+				return err;
+			}
+
+			first_fragment = false;
+		}
+
+		err = dfu_ctx_write(event->fragment.buf, event->fragment.len);
 		if (err != 0) {
-			LOG_ERR("flash_img_buffered_write error %d", err);
+			LOG_ERR("dfu_ctx_write error %d", err);
+			err = download_client_disconnect(&dlc);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
-		break;
+	break;
 	}
 
 	case DOWNLOAD_CLIENT_EVT_DONE:
-		/* Write with 0 length to flush the write operation to flash. */
-		err = flash_img_buffered_write(&flash_img,
-				(u8_t *)event->fragment.buf,
-				0, true);
+		err = dfu_ctx_done();
 		if (err != 0) {
-			LOG_ERR("flash_img_buffered_write error %d", err);
+			LOG_ERR("dfu_ctx_done error: %d", err);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
 
-		err = boot_request_upgrade(BOOT_UPGRADE_TEST);
-		if (err != 0) {
-			LOG_ERR("boot_request_upgrade error %d", err);
-			callback(FOTA_DOWNLOAD_EVT_ERROR);
-			return err;
-		}
-		err = download_client_disconnect(&dfu);
+		err = download_client_disconnect(&dlc);
 		if (err != 0) {
 			LOG_ERR("download_client_disconncet error %d", err);
 			callback(FOTA_DOWNLOAD_EVT_ERROR);
 			return err;
 		}
-		callback(FOTA_DOWNLOAD_EVT_FINISHED);
+		first_fragment = true;
 		break;
 
 	case DOWNLOAD_CLIENT_EVT_ERROR: {
-		download_client_disconnect(&dfu);
+		download_client_disconnect(&dlc);
+		first_fragment = true;
 		LOG_ERR("Download client error");
 		callback(FOTA_DOWNLOAD_EVT_ERROR);
 		return event->error;
@@ -96,6 +98,9 @@ static int download_client_callback(const struct download_client_evt *event)
 
 int fota_download_start(char *host, char *file)
 {
+	int err = -1;
+
+	/* TODO: add configurability of the .sec_tag for TLS certificates */
 	struct download_client_cfg config = {
 		.sec_tag = -1, /* HTTP */
 	};
@@ -104,29 +109,27 @@ int fota_download_start(char *host, char *file)
 		return -EINVAL;
 	}
 
+	/* TODO: Is it possible to download modem fw with a threaded model from
+	 * a host while downloading firmware that's written to flash?
+	 */
+
 	/* Verify that a download is not already ongoing */
-	if (dfu.fd != -1) {
+	if (dlc.fd != -1) {
 		return -EALREADY;
 	}
 
-	int err = flash_img_init(&flash_img);
-
-	if (err != 0) {
-		LOG_ERR("flash_img_init error %d", err);
-		return err;
-	}
-
-	err = download_client_connect(&dfu, host, &config);
+	err = download_client_connect(&dlc, host, &config);
 
 	if (err != 0) {
 		LOG_ERR("download_client_connect error %d", err);
 		return err;
 	}
 
-	err = download_client_start(&dfu, file, 0);
+
+	err = download_client_start(&dlc, file, 0);
 	if (err != 0) {
 		LOG_ERR("download_client_start error %d", err);
-		download_client_disconnect(&dfu);
+		download_client_disconnect(&dlc);
 		return err;
 	}
 	return 0;
@@ -140,12 +143,13 @@ int fota_download_init(fota_download_callback_t client_callback)
 
 	callback = client_callback;
 
-	int err = download_client_init(&dfu, download_client_callback);
+	int err = download_client_init(&dlc, download_client_callback);
 
 	if (err != 0) {
 		LOG_ERR("download_client_init error %d", err);
 		return err;
 	}
+
 
 	return 0;
 }
