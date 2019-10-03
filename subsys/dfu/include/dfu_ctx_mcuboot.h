57c57a5d7495f917d663035d58b9b28f546d3ee8
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2019-10-03 08:18:34 +0000

    move context types to dfu_context_handler
    There is no reason to have them in the lower layer files.
    This change makes it easier to re-use functions across the lower layer files.
    Now we can delete the internal header file as well.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/dfu/include/dfu_ctx_mcuboot.h
+++ subsys/dfu/include/dfu_ctx_mcuboot.h
@@ -7,16 +7,11 @@
 #define _DFU_CTX_MCUBOOT_H_
 
 #include <zephyr/types.h>
-#include "dfu_context_handler_internal.h"
 
 int dfu_ctx_mcuboot_init(void);
-int dfu_ctx_mcuboot_write(const void *const buf, size_t len);
-int dfu_ctx_mcuboot_done(void);
 
-static struct dfu_ctx dfu_ctx_mcuboot = {
-	.init  = dfu_ctx_mcuboot_init,
-	.write = dfu_ctx_mcuboot_write,
-	.done  = dfu_ctx_mcuboot_done,
-};
+int dfu_ctx_mcuboot_write(const void *const buf, size_t len);
+
+int dfu_ctx_mcuboot_done(void);
 
 #endif /* _DFU_CTX_MCUBOOT_H_ */
--- subsys/dfu/include/dfu_ctx_modem.h
+++ subsys/dfu/include/dfu_ctx_modem.h
@@ -7,15 +7,11 @@
 #define _DFU_CTX_MODEM_H_
 
 #include <zephyr/types.h>
-#include "dfu_context_handler_internal.h"
-int dfu_ctx_modem_init(void);
-int dfu_ctx_modem_write(const void *const buf, size_t len);
-int dfu_ctx_modem_done(void);
 
-static struct dfu_ctx dfu_ctx_modem = {
-	.init = dfu_ctx_modem_init,
-	.write = dfu_ctx_modem_write,
-	.done = dfu_ctx_modem_done,
-};
+int dfu_ctx_modem_init(void);
+
+int dfu_ctx_modem_write(const void *const buf, size_t len);
+
+int dfu_ctx_modem_done(void);
 
 #endif /* _DFU_CTX_MODEM_H_ */
