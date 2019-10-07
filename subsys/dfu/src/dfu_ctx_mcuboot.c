20b24faf62f839af6613a9ce046896a1f348d42b
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2019-10-07 13:23:33 +0000

    add offset for mcuboot type updates
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    
    modem update
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/dfu/src/dfu_ctx_mcuboot.c
+++ subsys/dfu/src/dfu_ctx_mcuboot.c
@@ -12,29 +12,43 @@
 LOG_MODULE_REGISTER(dfu_ctx_mcuboot, CONFIG_DFU_CTX_LOG_LEVEL);
 static struct flash_img_context flash_img;
 static size_t offset;
-static bool initialized = false;
 
 int dfu_ctx_mcuboot_init(void)
 {
-	if (!initialized) {
-		int err = flash_img_init(&flash_img);
+	int err = flash_img_init(&flash_img);
 
-		if (err != 0) {
-			LOG_ERR("flash_img_init error %d", err);
-			return err;
-		}
-
-		offset = 0;
-		initialized = true;
+	if (err != 0) {
+		LOG_ERR("flash_img_init error %d", err);
+		return err;
 	}
+
+	offset = 0;
+
+	return 0;
+}
+
+int dfu_ctx_mcuboot_offset(void)
+{
+
+	return offset;
+}
+
+int dfu_ctx_mcuboot_write(const void *const buf, size_t len)
+{
+	int err = flash_img_buffered_write(&flash_img, (u8_t *)buf, len, false);
+
+	if (err != 0) {
+		LOG_ERR("flash_img_buffered_write error %d", err);
+		return err;
+	}
+
+	offset += len;
 
 	return 0;
 }
 
 int dfu_ctx_mcuboot_done(void)
 {
-	__ASSERT_NO_MSG(initialized);
-
 	int err = flash_img_buffered_write(&flash_img, NULL,
 				       0, true);
 	if (err != 0) {
@@ -50,31 +64,6 @@ int dfu_ctx_mcuboot_done(void)
 
 	LOG_INF("MCUBoot image upgrade scheduled. Reset the device to apply");
 
-	/* Reset state to ensure another dfu process can be completed */
-	initialized = false;
-
 	return 0;
 }
 
-int dfu_ctx_mcuboot_offset(void)
-{
-	__ASSERT_NO_MSG(initialized);
-
-	return offset;
-}
-
-int dfu_ctx_mcuboot_write(const void *const buf, size_t len)
-{
-	__ASSERT_NO_MSG(initialized);
-
-	int err = flash_img_buffered_write(&flash_img, (u8_t *)buf, len, false);
-
-	if (err != 0) {
-		LOG_ERR("flash_img_buffered_write error %d", err);
-		return err;
-	}
-
-	offset += len;
-
-	return 0;
-}
