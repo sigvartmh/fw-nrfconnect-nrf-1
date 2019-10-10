132a3a09d0f7ae9745dbb7b1e032a714d3f28e94
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-10-10 10:56:45 +0200

    samples: nrf9160: http_application_update: Add modem DFU support
    This initializes `bsdlib` after sys_init to check the error code so that
    we can check if a modem upgrade was successful or not.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>

--- samples/nrf9160/http_application_update/src/main.c
+++ samples/nrf9160/http_application_update/src/main.c
@@ -6,12 +6,14 @@
 #include <stdint.h>
 #include <zephyr.h>
 #include <flash.h>
+#include <bsd.h>
 #include <nrf_socket.h>
 #include <logging/log.h>
 #include <gpio.h>
 #include <net/fota_download.h>
 #include <dfu/mcuboot.h>
 #include <lte_lc.h>
+#include <at_cmd.h>
 
 #define LED_PORT	DT_ALIAS_LED0_GPIOS_CONTROLLER
 
@@ -42,12 +44,14 @@ static void app_dfu_transfer_start(struct k_work *unused)
 	retval = fota_download_start(CONFIG_DOWNLOAD_HOST,
 				     CONFIG_DOWNLOAD_FILE);
 	if (retval != 0) {
+		/* Re-enable button callback */
+		gpio_pin_enable_callback(gpiob, DT_ALIAS_SW0_GPIOS_PIN);
+
 		printk("fota_download_start() failed, err %d\n",
 			retval);
 	}
-
-	return;
 }
+
 /**@brief Turn on LED0 and LED1 if CONFIG_APPLICATION_VERSION
  * is 2 and LED0 otherwise.
  */
@@ -109,12 +113,12 @@ static int dfu_button_init(void)
 void fota_dl_handler(enum fota_download_evt_id evt_id)
 {
 	switch (evt_id) {
+	case FOTA_DOWNLOAD_EVT_ERROR:
+		printk("Received error from fota_download\n");
+		/* Fallthrough */
 	case FOTA_DOWNLOAD_EVT_FINISHED:
 		/* Re-enable button callback */
 		gpio_pin_enable_callback(gpiob, DT_ALIAS_SW0_GPIOS_PIN);
-		break;
-	case FOTA_DOWNLOAD_EVT_ERROR:
-		printk("Received error from fota_download\n");
 		break;
 
 	default:
@@ -136,6 +140,8 @@ static void modem_configure(void)
 	} else {
 		int err;
 
+		err = at_cmd_init();
+		__ASSERT(err == 0, "AT CMD could not be established.");
 		printk("LTE Link Connecting ...\n");
 		err = lte_lc_init_and_connect();
 		__ASSERT(err == 0, "LTE link could not be established.");
@@ -171,6 +177,29 @@ static int application_init(void)
 void main(void)
 {
 	int err;
+	printk("Initializing bsdlib\n");
+	err = bsd_init();
+	switch (err) {
+	case MODEM_DFU_RESULT_OK:
+		printk("Modem firmware update successful!\n");
+		printk("Modem will run the new firmware after reboot\n");
+		k_thread_suspend(k_current_get());
+		break;
+	case MODEM_DFU_RESULT_UUID_ERROR:
+	case MODEM_DFU_RESULT_AUTH_ERROR:
+		printk("Modem firmware update failed\n");
+		printk("Modem will run non-updated firmware on reboot.\n");
+		break;
+	case MODEM_DFU_RESULT_HARDWARE_ERROR:
+	case MODEM_DFU_RESULT_INTERNAL_ERROR:
+		printk("Modem firmware update failed\n");
+		printk("Fatal error.\n");
+		break;
+
+	default:
+		break;
+	}
+	printk("Initialized bsdlib\n");
 
 	modem_configure();
 
@@ -182,6 +211,4 @@ void main(void)
 	}
 
 	printk("Press Button 1 to start the FOTA download\n");
-
-	return;
 }
