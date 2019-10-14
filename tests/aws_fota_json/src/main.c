4c91a36e56c93a9bd064b32cc4ea26d1e4df7895
Sigvart Hovland <sigvart.m@gmail.com>
2019-10-14 13:01:44 +0200

    aws fota modem test fix

--- tests/aws_fota_json/src/main.c
+++ tests/aws_fota_json/src/main.c
@@ -15,11 +15,12 @@ static void test_notify_next(void)
 	char expected_hostname[] = "fota-update-bucket.s3.eu-central-1.amazonaws.com";
 	char expected_file_path[] = "/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host";
 
-	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":1,\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
+	char encoded[] = "{\"timestamp\":1559808907,\"execution\":{\"jobId\":\"9b5caac6-3e8a-45dd-9273-c1b995762f4a\",\"status\":\"QUEUED\",\"queuedAt\":1559808906,\"lastUpdatedAt\":1559808906,\"versionNumber\":\"1\",\"executionNumber\":1,\"jobDocument\":{\"operation\":\"app_fw_update\",\"fwversion\":\"2\",\"size\":181124,\"location\":{\"protocol\":\"https:\",\"host\":\"fota-update-bucket.s3.eu-central-1.amazonaws.com\",\"path\":\"/update.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXEL53DXIU7W72AE%2F20190606%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20190606T081505Z&X-Amz-Expires=604800&X-Amz-Signature=913e00b97efe5565a901df4ff0b87e4878a406941d711f59d45915035989adcc&X-Amz-SignedHeaders=host\"}}}}";
 	int ret;
 	char job_id[100];
 	char hostname[100];
 	char file_path[1000];
+	int doc_version;
 
 	/* Memset to ensure correct null-termination */
 	memset(job_id, 0xff, sizeof(job_id));
@@ -27,11 +28,12 @@ static void test_notify_next(void)
 	memset(file_path, 0xff, sizeof(file_path));
 
 	ret = aws_fota_parse_notify_next_document(encoded, sizeof(encoded) - 1,
-			job_id, hostname, file_path);
+			job_id, hostname, file_path, &doc_version);
 
 	zassert_true(!strcmp(job_id, expected_job_id), NULL);
 	zassert_true(!strcmp(hostname, expected_hostname), NULL);
 	zassert_true(!strcmp(file_path, expected_file_path), NULL);
+	zassert_equal(doc_version, 1, NULL);
 }
 
 static void test_update_job_longer_than_max(void)
@@ -43,12 +45,14 @@ static void test_update_job_longer_than_max(void)
 			  "\"1\",\"clientToken\": \"\"}";
 	char status[100];
 	int ret;
+	int doc_version;
 
 	memset(status, 0xff, sizeof(status));
 
 	ret = aws_fota_parse_update_job_exec_state_rsp(encoded,
-			sizeof(encoded) - 1, status);
+			sizeof(encoded) - 1, status, &doc_version);
 	zassert_true(!strcmp(status, expected_status), NULL);
+	zassert_equal(doc_version, 1, NULL);
 }
 
 
@@ -59,9 +63,10 @@ static void test_timestamp_only(void)
 	char hostname[100];
 	char file_path[100];
 	char encoded[] = "{\"timestamp\":1559808907}";
+	int doc_version;
 
 	ret = aws_fota_parse_notify_next_document(encoded, sizeof(encoded) - 1,
-			job_id, hostname, file_path);
+			job_id, hostname, file_path, &doc_version);
 
 	zassert_equal(ret, 1,
 		     "Timestamp decoded correctly");
@@ -72,9 +77,10 @@ static void test_update_job_exec_rsp_minimal(void)
 	char encoded[] = "{\"timestamp\":4096,\"clientToken\":\"token\"}";
 	char status[100];
 	int ret;
+	int doc_version;
 
 	ret = aws_fota_parse_update_job_exec_state_rsp(encoded,
-			sizeof(encoded) - 1, status);
+			sizeof(encoded) - 1, status, &doc_version);
 
 	/* Only two last fields are set */
 	zassert_equal(ret, 0b11000, "All fields decoded correctly");
@@ -86,22 +92,24 @@ static void test_update_job_exec_rsp(void)
 	char encoded[] = "{\"status\":\"IN_PROGRESS\",\"statusDetails\":{\"nextState\":\"download_firmware\"},\"expectedVersion\":\"1\",\"clientToken\": \"\"}";
 	char status[100];
 	int ret;
+	int doc_version;
 
 	memset(status, 0xff, sizeof(status));
 
 	ret = aws_fota_parse_update_job_exec_state_rsp(encoded,
-			sizeof(encoded) - 1, status);
+			sizeof(encoded) - 1, status, &doc_version);
 	zassert_true(!strcmp(status, expected_status), NULL);
+	zassert_equal(doc_version, 1, NULL);
 }
 
 
 void test_main(void)
 {
 	ztest_test_suite(lib_json_test,
-			 ztest_unit_test(test_update_job_exec_rsp_minimal),
 			 ztest_unit_test(test_update_job_exec_rsp),
 			 ztest_unit_test(test_update_job_longer_than_max),
 			 ztest_unit_test(test_timestamp_only),
+			 ztest_unit_test(test_update_job_exec_rsp_minimal),
 			 ztest_unit_test(test_notify_next)
 			 );
 
