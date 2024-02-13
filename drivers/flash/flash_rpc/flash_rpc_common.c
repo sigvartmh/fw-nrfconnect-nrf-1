8bbf11d9d426b264496a088858bb6f0632896e0b
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2024-02-13 12:59:42 +0100

    driver: flash: Network core flash driver
    lash driver for interacting with network core's flash from the
    application core. This utilizes IPC and NRF_RPC to do flash commands in
    the network core.
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>

---
+++ drivers/flash/flash_rpc/flash_rpc_common.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+#include <nrf_rpc/nrf_rpc_ipc.h>
+#include <nrf_rpc_cbor.h>
+
+LOG_MODULE_REGISTER(FLASH_RPC, CONFIG_FLASH_RPC_LOG_LEVEL);
+
+#ifdef CONFIG_FLASH_RPC_SYS_INIT
+static void err_handler(const struct nrf_rpc_err_report *report)
+{
+	LOG_ERR("nRF RPC error %d ocurred. See nRF RPC logs for more details.",
+	       report->code);
+	k_oops();
+}
+
+static int serialization_init(void)
+{
+	int err;
+
+	LOG_INF("Init begin");
+
+	err = nrf_rpc_init(err_handler);
+	if (err) {
+		LOG_ERR("Something went wrong");
+		return -NRF_EINVAL;
+	}
+
+	LOG_INF("Init done");
+	return 0;
+}
+
+SYS_INIT(serialization_init, POST_KERNEL, CONFIG_FLASH_RPC_SYS_INIT_PRIORITY);
+#endif /* CONFIG_FLASH_RPC_SYS_INIT */
